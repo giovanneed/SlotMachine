3c9dd3c8b4f4fede28a956eb1e14811ae349579f
User <user@Users-MacBook-Pro.local>
2020-01-08 21:18:57 -0500

    first commit

--- SlotMachine/ViewController.swift
+++ SlotMachine/ViewController.swift
@@ -9,19 +9,57 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var playerMoney : Int = 10000
+    var bet : Int = 50
 
     @IBOutlet weak var imageOne: UIImageView!
     @IBOutlet weak var imageTwo: UIImageView!
     @IBOutlet weak var imageThree: UIImageView!
+    @IBOutlet weak var playerMoneyLabel: UILabel!
     
+    @IBOutlet weak var betMoney: UILabel!
     @IBOutlet weak var userMoney: UILabel!
     
     @IBAction func Spin(_ sender: AnyObject) {
         imageOne.image = UIImage(named: reels())
         imageTwo.image = UIImage(named: reels())
         imageThree.image = UIImage(named: reels())
+        
+        var result = 0
+        //check if win
+        if imageOne.image == imageTwo.image, imageTwo.image == imageThree.image {
+            print("win")
+            result = playerMoney + bet
+
+        } else {
+            print("lose")
+            result = playerMoney - bet
+            
+        }
+        
+        playerMoney = result
+        
+        playerMoneyLabel.text = String(result)
+
     }
     
+    @IBAction func bet50(_ sender: AnyObject) {
+        betMoney.text = "50"
+        bet=50
+    }
+    @IBAction func bet100(_ sender: AnyObject) {
+        betMoney.text = "100"
+        bet=100
+    }
+    @IBAction func bet500(_ sender: AnyObject) {
+        betMoney.text = "500"
+        bet=500
+    }
+    @IBAction func bet1k(_ sender: AnyObject) {
+        betMoney.text = "1000"
+        bet=1000
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -31,6 +69,7 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
     
     func reels()-> String{
         
